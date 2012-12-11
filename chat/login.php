4f7e205aea9446f737b16063675120bf963422af
Niklas Åsberg <niklas.asberg@webbits.nu>
2012-12-11 17:01:23 +0100

    såå ja

--- chat/login.php
+++ chat/login.php
@@ -10,7 +10,27 @@ $connect = mysql_connect("localhost","root","root") or die("NO!");
 mysql_select_db("phplogin") or die("Wrong!");
 
 $query = mysql_query("SELECT * FROM users WHERE username='$username'");
+
 $numrows = mysql_num_rows($query);
+
+if ($numrows!=0) {
+
+	while ($row = mysql_fetch_assoc($query)) 
+	{
+		$dbusername = $row ['username'];
+		$dbpassword = $row ['password'];
+		# code...
+	}
+	if ($username==$dbusername&&$password==$dbpassword) 
+	{
+		echo "grattis :D";
+	}
+	else
+		echo "Feeeeel!!";
+}
+else
+	die("$username is not a user");
+
 
 
 }
