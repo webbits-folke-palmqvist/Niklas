5a4da4366c5b3f67cf51a4a78ecee9f183e18702
Niklas Åsberg <niklas.asberg@webbits.nu>
2012-12-11 16:38:34 +0100

    Nu ja!

--- chat/login.php
+++ chat/login.php
@@ -1,4 +1,20 @@
 <?php
+
 $username = $_POST['username'];
 $password = $_POST['password'];
+
+if ($username&&$password)
+{
+
+$connect = mysql_connect("localhost","root","root") or die("NO!");
+mysql_select_db("phplogin") or die("Wrong!");
+
+$query = mysql_query("SELECT * FROM users WHERE username='$username'");
+$numrows = mysql_num_rows($query);
+
+
+}
+else
+	die("Skriv!");
+
 ?>
